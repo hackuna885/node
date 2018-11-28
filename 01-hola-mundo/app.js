2745a9e2f062637c0b84dad890ebe55d97ec9884
hackuna885 <hackuna885@gmail.com>
2018-11-28 07:45:52 -0600

    Initial commit

---
+++ 01-hola-mundo/app.js
@@ -0,0 +1,4 @@
+let nombre = 'hackuna';
+
+
+console.log(`Hola ${ nombre }`);
---
+++ 01-hola-mundo/app3.js
@@ -0,0 +1,15 @@
+console.log('Inicio del PRograma');
+
+setTimeout(function() {
+    console.log('Primer TimeOut');
+}, 3000);
+
+setTimeout(function() {
+    console.log('Segundo TimeOut');
+}, 0);
+
+setTimeout(function() {
+    console.log('Tercer TimeOut');
+}, 0);
+
+console.log('Fin del PRograma');
